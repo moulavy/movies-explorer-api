e1fab34693fc5376374e7346622234dfc2bf1ed0
Milana Kulieva <72352010+moulavy@users.noreply.github.com>
2023-05-27 19:58:35 +0300

    создание модели movie

--- models/movie.js
+++ models/movie.js
@@ -1,24 +1,69 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const regexURL=required('../utils/regex')
 
-const userSchema = new mongoose.Schema(
+const movieSchema = new mongoose.Schema(
   {
-    email: {
+    country: {
       type: String,
-      required: true,
-      unique: true,
-      validate:[validator.isEmail,'Неправильный формат email.']
+      required:true
     },
-    password: {
+    director: {
       type: String,
-      required: true,
-      select:false
+      required:true
     },
-    name: {
+    duration: {
+      type: Number,
+      required:true,
+    },
+    year: {
+      type:String,
+      required:true,
+    },
+    description: {
       type: String,
       required:true,
-      minlength: 2,
-      maxlength:30
+    },
+    image: {
+      type: String,
+      required: true,
+      validator: {
+        validator: (value) => regexURL.test(value),
+        message:'Неправильный формат ссылки'
+      }
+    },
+    trailerLink: {
+      type: String,
+      required: true,
+      validator: {
+        validator: (value) => regexURL.test(value),
+        message: 'Неправильный формат ссылки'
+      }
+    },
+    thumbnail: {
+      type: String,
+      required: true,
+      validator: {
+        validator: (value) => regexURL.test(value),
+        message: 'Неправильный формат ссылки'
+      }
+    },
+    owner: {
+      type: String,
+      required: true,
+      ref:user
+    },
+    movieId: {
+      type: Number,
+      required:true
+    },
+    nameRU: {
+      type: String,
+      required:true
+    },
+    nameEN: {
+      type: String,
+      required:true
     }
   }
 )
