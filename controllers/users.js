e26b4f6192f6271a06ea2cad810fb05d1b96e5c7
Milana Kulieva <72352010+moulavy@users.noreply.github.com>
2023-05-28 13:32:55 +0300

    контроллеры и роуты createUser, login,logout

--- controllers/users.js
+++ controllers/users.js
@@ -4,7 +4,45 @@ const jwt = require('jsonwebtoken');
 const NotFoundError = require('../errors/NotFoundError');
 const BadRequestError = require('../errors/BadRequestError');
 const ConflictError = require('../errors/ConflictError');
+const { NODE_ENV, JWT_SECRET } = process.env;
 
+module.exports.createUser = (req, res, next) => {
+  const { email, password, name } = req.body;
+  bcrypt.hash(password, 10)
+    .then((hash) => User.create({
+    email,password:hash,name
+    }))
+    .then((user) => {
+      res.status(201).send({ data: user });
+    })
+    .catch((err) => {
+      if (err.code === 11000) {
+        return next(new ConflictError('Пользователь с таким email уже существует'));
+      } if (err instanceof ValidationError) {
+        return next(new BadRequestError('Переданы некорректные данные при создании пользователя.'));
+      }
+      return next(err);
+  })
+}
+
+module.exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+  User.findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'secret-key-123', { expiresIn: '7d' });
+      res.cookie('token', token, {
+        maxAge: 3600000 * 24 * 7,
+        httpOnly: true, // чтобы к кукам не было доступа из JavaScript
+        sameSite: true,//запрос исходит от того же домена
+      })
+        .send({ message: "Успешно вошли!" });
+    })
+    .catch(next);
+};
+
+module.exports.logout = (req, res) => {
+  res.clearCookie('token').send('Вышли');
+};
 
 //возвращает информацию о пользователе
 module.exports.getInfoUser = (req, res, next) => {
---
+++ routes/index.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+const { login, createUser, logout } = require('../controllers/users');
+const userRouter = require('./users');
+const movieRouter = require('./movies');
+
+router.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}), login);
+
+router.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+    name: Joi.string().min(2).max(30)
+  }),
+}), createUser);
+
+router.use(auth);
+
+router.use('/users', userRouter);
+router.use('/movies', movieRouter);
+router.use('/signout', logout);
+
+router.all('*', (req, res, next) => {
+  next(new NotFoundError('Страницы не существует.'));
+});
+
+module.exports = router;
