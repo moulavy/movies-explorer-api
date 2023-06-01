9a1e2fae8006b49141741500c4beba02da565001
Milana Kulieva <72352010+moulavy@users.noreply.github.com>
2023-06-01 16:16:16 +0300

    mongo_url из process.env

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ const mainRouter = require('./routes/index');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const corsCheck = require('./middlewares/cors');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, NODE_ENV, MONGO_URL } = process.env;
 const app = express();
 
-mongoose.connect('mongodb://127.0.0.1:27017/bifilmsdb', {
+mongoose.connect(NODE_ENV==='production' ? MONGO_URL : 'mongodb://127.0.0.1:27017/bitfilmsdb', {
   useNewUrlParser: true,
 });
 
