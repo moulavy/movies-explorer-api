c3d3ab0702d340016df3178735cc7e7d33e6d7e2
Milana Kulieva <72352010+moulavy@users.noreply.github.com>
2023-06-01 15:49:57 +0300

    исправлено:убрано ограничение длины пароля и поле name обязательное при регистрации

--- utils/validate/validateUser.js
+++ utils/validate/validateUser.js
@@ -10,14 +10,14 @@ module.exports.validateUpdateUser = celebrate({
 module.exports.validateSignIn = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
+    password: Joi.string().required(),
   }),
 });
 
 module.exports.validateSignUp = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
-    name: Joi.string().min(2).max(30),
+    password: Joi.string().required(),
+    name: Joi.string().min(2).max(30).required(),
   }),
 });
