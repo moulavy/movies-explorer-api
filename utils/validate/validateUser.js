48044902c597be11bce982b2f0dcad75bf03845a
Milana Kulieva <72352010+moulavy@users.noreply.github.com>
2023-06-21 13:53:28 +0300

    Update validateUser.js

--- utils/validate/validateUser.js
+++ utils/validate/validateUser.js
@@ -2,21 +2,21 @@ const { celebrate, Joi } = require('celebrate');
 
 module.exports.validateUpdateUser = celebrate({
   body: Joi.object().keys({
-    email: Joi.string().email().required(),
+    email: Joi.string().email().required().error(new Error('Пожалуйста, введите корректный email')),
     name: Joi.string().min(2).max(30).required(),
   }),
 });
 
 module.exports.validateSignIn = celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required().email(),
+    email: Joi.string().required().email().error(new Error('Пожалуйста, введите корректный email')),
     password: Joi.string().required(),
   }),
 });
 
 module.exports.validateSignUp = celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required().email(),
+    email: Joi.string().required().email().error(new Error('Пожалуйста, введите корректный email')),
     password: Joi.string().required(),
     name: Joi.string().min(2).max(30).required(),
   }),
