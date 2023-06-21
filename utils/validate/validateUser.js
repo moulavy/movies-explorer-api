f989d3dee6e41fc773b53246da5517c8cc257670
Milana Kulieva <72352010+moulavy@users.noreply.github.com>
2023-06-21 14:33:50 +0300

    Update validateUser.js

--- utils/validate/validateUser.js
+++ utils/validate/validateUser.js
@@ -2,21 +2,36 @@ const { celebrate, Joi } = require('celebrate');
 
 module.exports.validateUpdateUser = celebrate({
   body: Joi.object().keys({
-    email: Joi.string().email().required().error(new Error('Пожалуйста, введите корректный email')),
+    email: Joi.string().email().required().error((errors) => {
+      errors.forEach((err) => {
+        err.message = 'Пожалуйста, введите корректный email';
+      });
+      return errors;
+    }),
     name: Joi.string().min(2).max(30).required(),
   }),
 });
 
 module.exports.validateSignIn = celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required().email().error(new Error('Пожалуйста, введите корректный email')),
+    email: Joi.string().required().email().error((errors) => {
+      errors.forEach((err) => {
+        err.message = 'Пожалуйста, введите корректный email';
+      });
+      return errors;
+    }),
     password: Joi.string().required(),
   }),
 });
 
 module.exports.validateSignUp = celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required().email().error(new Error('Пожалуйста, введите корректный email')),
+    email: Joi.string().required().email().error((errors) => {
+      errors.forEach((err) => {
+        err.message = 'Пожалуйста, введите корректный email';
+      });
+      return errors;
+    }),
     password: Joi.string().required(),
     name: Joi.string().min(2).max(30).required(),
   }),
